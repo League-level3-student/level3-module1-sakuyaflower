0cc45a8f223afc85e3370fd6bb1af457f7240b6f
root <root@5ffd458a2b40>
2020-12-19 02:28:06 +0000

    Intro To Stack

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -17,9 +17,18 @@ public class _01_IntroToStack {
 			numbers.push(num);
 		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		String answer = JOptionPane.showInputDialog(null,"Choose two number between 0 to 100. ");
+		String answer = JOptionPane.showInputDialog(null,"Pick one number between 0 to 50. ");
+		String answerTwo = JOptionPane.showInputDialog(null,"Pick one number between 50 to 100. ");
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
+		int finalNum = Integer.parseInt(answer);
+		int secondNum = Integer.parseInt(answerTwo);
+		for(int i = 0; i < numbers.size(); i++) {
+		double num = numbers.pop();
+		if(){
+			
+		}
+		}
 		
 		
 		//   EXAMPLE:
