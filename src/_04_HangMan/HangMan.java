ba30551b18dce5acb9ff7da851429c53a9de7ed2
student <student@ip-172-31-59-128.us-west-2.compute.internal>
2021-02-26 18:27:55 -0800

    Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,45 @@
 package _04_HangMan;
 
-public class HangMan{
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class HangMan implements ActionListener{
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	JButton button = new JButton("Start Game");
+	ArrayList<String> 
 	
+public static void main(String[] args) {
+HangMan game = new HangMan();
+game.setup();
+}	
 	
+void setup() {
+	frame.add(panel);
+	panel.add(label);
+	frame.setVisible(true);
+	panel.add(button);
+	button.addActionListener(this);
 	
+}
+void theHangManGame() {
+	
+}
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton pressed = (JButton) e.getSource();
+	if(button == pressed) {
+		String answer = JOptionPane.showInputDialog(null,"Pick a number from 1-266");
+		int finalNumber = Integer.parseInt(answer);
+	}
+}
 }
