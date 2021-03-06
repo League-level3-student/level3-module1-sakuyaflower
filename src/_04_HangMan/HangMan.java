1c52b07e3ebc97c1efa6d20472964e4b9080e311
root <root@bc590042dff5>
2021-03-06 02:23:26 +0000

    Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,6 +2,7 @@ package _04_HangMan;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,10 +15,10 @@ public class HangMan implements ActionListener{
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
 	JButton button = new JButton("Start Game");
-	ArrayList<String> 
-	
+	ArrayList<String> phrase = new ArrayList<String>();  
+	static HangMan game = new HangMan();
 public static void main(String[] args) {
-HangMan game = new HangMan();
+
 game.setup();
 }	
 	
@@ -38,8 +39,11 @@ public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	JButton pressed = (JButton) e.getSource();
 	if(button == pressed) {
-		String answer = JOptionPane.showInputDialog(null,"Pick a number from 1-266");
+		String answer = JOptionPane.showInputDialog(null,"Pick a number up too " + getTotalWordsInFile("dictionary.txt"));
 		int finalNumber = Integer.parseInt(answer);
+		for(int i = 0; i < phrase.size(); i++) {
+			readRandomLineFromFile("dictionary.txt"); 
+	}
 	}
 }
 }
