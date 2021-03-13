c8ed8286c5b3e83c96c19dc1f61aa2ede94459d3
student <student@ip-172-31-60-239.us-west-2.compute.internal>
2021-03-12 18:25:49 -0800

    Hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -2,7 +2,10 @@ package _04_HangMan;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Stack;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,40 +13,88 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan implements ActionListener{
+public class HangMan implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JLabel label = new JLabel();
-	JButton button = new JButton("Start Game");
-	ArrayList<String> phrase = new ArrayList<String>();  
+	Stack<String> words = new Stack<String>();
 	static HangMan game = new HangMan();
-public static void main(String[] args) {
+	String characWord;
 
-game.setup();
-}	
-	
-void setup() {
-	frame.add(panel);
-	panel.add(label);
-	frame.setVisible(true);
-	panel.add(button);
-	button.addActionListener(this);
-	
-}
+	public static void main(String[] args) {
+		game.theHangManGame();
+		game.setup();
+		game.getNextWord();
+	}
+
+	void setup() {
+		frame.add(panel);
+		panel.add(label);
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
 void theHangManGame() {
+	String answer = JOptionPane.showInputDialog(null,"Pick a number up too " + Utilities.getTotalWordsInFile("dictionary.txt"));	
+	int finalNumber = Integer.parseInt(answer);
+	for(int i = 0; i < finalNumber; i++) {
+	String word = Utilities.readRandomLineFromFile("dictionary.txt");
+	if(!words.contains(word)) {
+		words.push(word);
+	}
+}
+}
+void getNextWord() {
+	String labelText = "";
+if(!words.isEmpty()) {
+	characWord = words.pop();
+	for(int i = 0; i < characWord.length(); i++) {
+		labelText += "_ ";
+	}
+	label.setText(labelText);
+	frame.pack();
+	System.out.println(characWord);
+}
+}
+
+@Override
+public void keyPressed(KeyEvent arg0) {
+	// TODO Auto-generated method stub
+	char pressed = arg0.getKeyChar();
+	String newLabel = "";
+	if(!characWord.contains("" + pressed)) {
+		//lose a life in the game
+		return;
+	}
+	else {
+		for(int i = 0; i < characWord.length(); i++) {
+			if(characWord.charAt(i) == pressed) {
+				newLabel += pressed;
+			}
+			else {
+				newLabel += label.getText().charAt(i*2);
+				
+			}
+			newLabel += " ";
+		}	
+	label.setText(newLabel);
+	frame.pack();
+	}
+//if words have been guess move on to the next
+	//lose a life
+	//add a stack for letters that are incorrect
+}
+
+@Override
+public void keyReleased(KeyEvent arg0) {
+	// TODO Auto-generated method stub
 	
 }
 
 @Override
-public void actionPerformed(ActionEvent e) {
+public void keyTyped(KeyEvent arg0) {
 	// TODO Auto-generated method stub
-	JButton pressed = (JButton) e.getSource();
-	if(button == pressed) {
-		String answer = JOptionPane.showInputDialog(null,"Pick a number up too " + getTotalWordsInFile("dictionary.txt"));
-		int finalNumber = Integer.parseInt(answer);
-		for(int i = 0; i < phrase.size(); i++) {
-			readRandomLineFromFile("dictionary.txt"); 
-	}
-	}
+	
 }
 }
