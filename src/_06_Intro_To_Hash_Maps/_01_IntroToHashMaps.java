bd2eb6834e57f9e306193d967adac0a4c8604653
student <student@ip-172-31-61-171.us-west-2.compute.internal>
2021-04-30 18:25:54 -0700

    Intro to Hash Maps

--- src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
+++ src/_06_Intro_To_Hash_Maps/_01_IntroToHashMaps.java
@@ -10,13 +10,17 @@ HashMap<Integer, Double> roots = new HashMap<Integer, Double>();
 		//2. Using a for-loop, add 500 entries to your HashMap. The key entry will be the 
 		//   current iteration of the loop (i). The value entry will be the square root of i.
 for(int i = 0; i < 500; i++) {
-	roots.put(i, sqrt(i));
+for(Integer y: roots.keySet()) {
+System.out.println(y);
+}
+for(Double d: roots.values()) {
+System.out.println(d);
 
 }
-
+}
 		//3. Iterate through all the entries in your HashMap displaying the keys with their respective
 		//   square roots (values). Use the following format.
-
+System.out.println();
 		//   The square Root of 0 is 0.0
 		//   The square Root of 1 is 1.0
 		//   The square Root of 2 is 1.4142135623730951
